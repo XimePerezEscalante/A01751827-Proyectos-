63ab2b8a82511415bd492b8c82c297628b392998
XimePerezEscalante <142125448+XimePerezEscalante@users.noreply.github.com>
2023-10-04 22:41:53 -0600

    Add files via upload

--- Calculo.py
+++ Calculo.py
@@ -2,205 +2,344 @@ import random
 
 from random import randint
 
-global a,b,c,derivadaTrig,intTrig
+from getkey import getkey, keys
 
-#Función para escoger una función trigonométrica
+global a,b,c
 
-def trigonometricas():
+#Función para definir el signo si la pregunta es una suma de derivadas.
 
-	lista_trig_deriv = ["sen","cos","tan","sec","cot","csc"]
+def func_signo():
 
-	opcion_trig = random.choice(lista_trig_deriv)
+	signo = ["+","-"]
 
-	return opcion_trig
-	
-#Función para escoger una función trigonométrica
+	signo_random = random.choice(signo)
 
-def trig_int():
+	return signo_random
 
-	lista_trig_int = ["sen","cos","sec**2","sectan","csc**2","csccot"]
+"""
+Función que regresa la ecuación a derivar, de la complejidad dependerá el número de elementos a derivar.
+a: coeficiente
+b: literal
+c: exponente
+"""
 
-	opcion_int = random.choice(lista_trig_int)
+def preguntas_deriv():
 
-	return opcion_int
-
-#Función para crear la ecuación a integrar
-
-def preguntas_deriv_int():
-
-	ecuacion = []
-
-	intTrig = trig_int()
-
-	a = randint(-9,9)
-
-	b = ""
-
-	c = randint(-9,9)
-
-	if intTrig == "sectan":
-
-		ecuacion.append(("sec(",a,"x",c,")tan(",a,"x",c,")"))
-
-		ecuacion.append((a,"x",c))
-
-	elif intTrig == "csccot":
-
-		ecuacion.append(("csc(",a,"x",c,")cot(",a,"x",c,")"))
-
-	else:
-
-		ecuacion.append((intTrig,a,"x",c))
-
-#Función para crear la ecuación a derivar
-
-def preguntas_deriv_trig():
+	complejidad = randint(1,3)
 
 	ecuacion = []
 
-	derivadaTrig = trigonometricas()
-
-	a = randint(-9,9)
-
-	b = ""
-
-	c = randint(-9,9)
-
-	while a == 0 or a == 1:
+	while complejidad > 0:
 
 		a = randint(-9,9)
 
-	while c == 1:
+		b = ""
 
 		c = randint(-9,9)
 
-	ecuacion.append((derivadaTrig,a,"x",c))
+		while a == 0:
+
+			a = randint(-9,9)
+
+		while a == 1:
+
+			a = randint(-9,9)
+
+		while c == 1:
+
+			c = randint(-9,9)
+
+		ecuacion.append((a,"x",c))
+
+		complejidad = complejidad - 1
 
 	return ecuacion
 
-#Función que deriva la ecuacion
+"""
+Función que deriva la ecuación, sólo recibe el coeficiente y el exponente para multiplicarlos
+ y restarle 1 al exponente, si el signo de la función func_signo es negativo, multiplica el coeficiente por -1.
+Si el exponente, el coeficiente o ambos son igual a 1, no los muestra.
+ """
 
-def respuesta_deriv_trig(d,a,b,c):
+def respuesta_deriv(e,lista_signo):
 
+	cont = 0
+	res = []
+	num = 0
+	cont2 = 0
+	cont3 = 0
 	resultado = ""
 
-	res = []
+	while cont <= len(e):
 
-	deriv_trig = {"sen":"cos","cos":"sen","tan":"sec**2","cot":"csc**2","sec":"sec(x)tan(x)","csc":"csc(x)cot(x)"}
+		for i in e:
 
-	if c == 0:
+			cont = cont + 1
 
-		resultado = 0
+			while num < len(e):
 
-	if c != 1 and c != 0:
-		
-		a1 = a * c
-		c1 = c - 1
+				#print("a")
 
-		if d == "cos" or d == "cot":
+				exp = e[num][1]
 
-			a1 = a1 * -1
+				coef = e[num][0]
 
-			res = [str(a1),str(b),"**",str(c1),deriv_trig[d],"(",str(a),str(b),"**",str(c),")"]
+				if exp == 0:
 
-		elif d == "csc":
+					if cont3 == 0 and len(e) == 1:
 
-			a1 = a1 * -1
+						res.append("0")
 
-			res = [str(a1),str(b),"**",str(c1),"csc(",str(a),str(b),"**",str(c),")cot(",str(a),str(b),"**",str(c),")"]
+				elif exp == 1:
 
-		elif d == "sec":
+					if len(e) > 1:
 
-			res = [str(a1),str(b),"**",str(c1),"sec(",str(a),str(b),"**",str(c),")tan(",str(a),str(b),"**",str(c),")"]
+						if cont3 == 1:
 
-		else:
+							signo2 = lista_signo[0]
 
-			res = [str(a1),str(b),"**",str(c1),deriv_trig[d],"(",str(a),str(b),"**",str(c),")"]
+							if signo2 == "-":
 
-		for i in res:
+								coef = coef * -1
 
-			resultado += i + "" 
+
+						elif cont3 == 2:
+
+							signo2 = lista_signo[1]
+
+							if signo2 == "-":
+
+								coef = coef * -1
+
+					else:
+
+						res.append(str(coef))
+
+				else:
+
+					exp = e[num][1] - 1
+
+					coef = e[num][0] * e[num][1]
+
+					if len(e) > 1:
+
+						if cont3 == 1:
+
+							signo2 = lista_signo[0]
+
+							if signo2 == "-":
+
+								coef = coef * -1
+
+
+						elif cont3 == 2:
+
+							signo2 = lista_signo[1]
+
+							if signo2 == "-":
+
+								coef = coef * -1
+
+
+					if cont2 > 0:
+
+						if coef > 0:
+
+							if exp == 1:
+
+								res.append(("+",str(coef),"x"))
+
+							else:
+
+								#res.append(("+",str(coef),"x**",str(exp)))
+								res.append(("+",str(coef),"x^",str(exp)))
+
+						else:
+
+							if exp == 1:
+
+								res.append((str(coef),"x"))
+
+							else:
+
+								#res.append((str(coef),"x**",str(exp)))
+								res.append((str(coef),"x^",str(exp)))
+
+					else:
+
+						#res.append((str(coef),"x**",str(exp)))
+
+						if exp == 1:
+
+							res.append((str(coef),"x"))
+
+						else:
+
+							#res.append((str(coef),"x**",str(exp)))
+							res.append((str(coef),"x^",str(exp)))
+
+
+					cont2 = cont2 + 1		
+
+				num = num + 1
+
+				cont3 = cont3 + 1
+
+	if len(res) > 0:
+
+		for x in res:
+
+				for g in x:
+
+					resultado += g + "" 
 
 	return str(resultado)
 
-#Función para mostrar distintos mensajes dependiendo el número de respuestas correctas que tuvo el usuario
+#Función para mostrar diferentes mensajes dependiendo el puntaje que haya obtenido el usuario.
 
 def calif(puntos):
 
 	if puntos == 10: 
-
-		print("¡Excelente!")
+		print("¡Excelente!\n--------------------------------")
 
 	elif puntos == 8:
-
-		print("¡Increíble!")
+		print("¡Increíble!\n--------------------------------")
 
 	elif puntos == 6:
-
-		print("¡Muy bien!")
+		print("¡Muy bien!\n--------------------------------")
 
 	elif puntos == 4:
-
-		print("¡Tú puedes!")
+		print("¡Tú puedes!\n--------------------------------")
 
 	elif puntos == 2:
-
-		print("¡No te rindas!")
+		print("¡No te rindas!\n--------------------------------")
 
 	else:
+		print("¡No pasa nada, la práctica hace al maestro!\n--------------------------------")
 
-		print("¡No pasa nada, la práctica hace al maestro!")
+"""
+Función para generar cada ronda de 5 preguntas, recibe la ecuación de la función preguntas_deriv,
+el signo de func_signo y los manda a respuesta_deriv, si la respuesta del usuario es igual a la
+respuesta de la función, se le suman dos puntos y al final se manda llamar a la función calif que 
+recibe los mismos como parámetro.
+"""
 
-#Función para crear cada ronda del juego
+def juego_deriv():
 
-def juego_trig():
-	cont = 5
+	#cont = 5
 	cont2 = 0
 	puntos = 0
+	signo_final = ""
 
-	while cont > 0:
+
+	while cont2 < 5:
 
 		cont2 = cont2 + 1
 
-		print("--------------------------------\nPregunta",cont2,":\nf(x) = ",end = "")
+		cont3 = 1
 
-		for n in preguntas_deriv_trig():
+		lista_signo = []
 
-			print(n[0],"(",n[1],n[2],"**",n[3],")")
+		lista = []
 
+		print("--------------------------------\nPregunta",cont2,":\n\nf(x) = ",end = "")
 
-		respuesta = input("\nf'(x) = ")
+		for n in preguntas_deriv():
 
-		if respuesta == respuesta_deriv_trig(n[0],n[1],n[2],n[3]):
-			print("\n¡Correcto!     +2pts")
+			if cont3 == 1:
+
+				#print(n[0],n[1],"**",n[2],end = " ")
+
+				print(n[0],n[1],"^",n[2],end = " ")
+
+				lista.append([n[0],n[2]])
+
+				cont3 = cont3 + 1
+
+			else:
+
+				signo_final = func_signo()
+
+				#print(" ",signo_final,"(",n[0],n[1],"**",n[2],")",end = "")
+
+				print(" ",signo_final,"(",n[0],n[1],"^",n[2],")",end = "")
+
+				lista.append([n[0],n[2]])
+
+			if n == "":
+
+				n = 1
+
+			lista_signo.append(signo_final) 
+
+			if len(lista_signo) == len(lista):
+
+				lista_signo.pop(0)			
+
+		respuesta = input("\n\nf'(x) = ")
+
+		if respuesta == respuesta_deriv(lista,lista_signo):
+			print("\n¡Correcto!     +2pts\n")
 			puntos = puntos + 2
+			
 
 		else:
 			print("\n¡Incorrecto!     +0pts")
 			puntos = puntos + 0
-			print("RESULTADO:",respuesta_deriv_trig(n[0],n[1],n[2],n[3]))
-		
-		cont = cont - 1
-	
-    
-	print("Puntos:",puntos)
+			print("RESULTADO:",respuesta_deriv(lista,lista_signo),"\n")
+
+		#cont = cont - 1
+
+	print("--------------------------------\nPuntos:",puntos,"\n")
 
 	calif(puntos)
 
+print("Temas:\n\n1. Derivación\n2. Integración\n\nEscoge el tema que te gustaría aprender (Escribe 1 o 2):\n")
 
-practicar = input("¿Deseas practicar este tema?\n\nPresiona la tecla S para practicar o la tecla N si deseas seguir leyendo o pasar a otro tema:\n")
+key = getkey()
 
-while practicar == "S" or practicar == "s":
+while key != "1" and key != "2":
+    tema = print("Escribe 1 o 2:\n")
+    key = getkey()
 
-	juego_trig()
+if key == "1":
 
-	practicar = input("¿Nuevo juego?\n\nS = Sí\n\nN = No\n")
+	print("DERIVACIÓN")
 
-print("De acuerdo")
+	print("\nDeseas practicar este tema?\n\nS = Sí\n\nN = No\n")
 
+	key = getkey()
 
+	if key == "n" or key == "N":
 
+		print("¡No hay problema!")
 
+	while key == "s" or key == "S":
 
+		juego_deriv()
+
+		print("\n¿Nuevo juego?\n\nS = Sí\n\nN = No\n")
+
+		key = getkey()
+
+		if key == "n" or key == "N":
+
+			print("¿Estás segur@?\n\nS = Sí\n\nN = No\n")
+
+			key = getkey()
+
+			if key == "s" or key == "S":
+
+				key = "n" 
+        
+    #Cuando el usuario decide dejar de practicar este tipo de derivadas, se prosigue a las trigonométricas que están en otro archivo.
+
+	print("¡De acuerdo!\n--------------------------------\n\nA continuación aprenderás a derivar funciones trigonométricas")
+
+	exec(open("./trigo.py").read())
+
+elif key == "2":
+
+	print("INTEGRACIÓN\n\nEn proceso...")
 
 
